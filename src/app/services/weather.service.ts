eb48ef030f20808df56ea19636cd5784223930cb
Sebastian <scelying@gmail.com>
2022-08-14 21:46:48 -0500

    Full weather app V1

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class WeatherService {
-  readonly options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '8895a30e96msh9b6ae83d4c51df2p1b1544jsn252c9d717f4f',
-      'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-    }
-  };
+  // readonly options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': '8895a30e96msh9b6ae83d4c51df2p1b1544jsn252c9d717f4f',
+  //     'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+  //   }
+  // };
   // private ROUTE_API_LOCATION = `https://api.ipgeolocation.io/ipgeo?apiKey=${this.ipGeoAPIKey}`;
   private ROUTE_API_LOCATION = 'https://json.geoiplookup.io/';
+  private OPEN_WEATHER_API = '6168bc99a8fb2d7db3a7f6f9d3090ac9';
+
+  // PRUEBA DE API OPEN WEATHER
+  // https://api.openweathermap.org/data/2.5/weather?lat=4.65124&lon=-74.1064&appid=6168bc99a8fb2d7db3a7f6f9d3090ac9
 
   constructor() { }
 
@@ -23,7 +28,7 @@ export class WeatherService {
   }
 
   async getWeatherData(latitude: string, longitude: string) {
-    const response = await fetch(`https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=${latitude}&lon=${longitude}`, this.options)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.OPEN_WEATHER_API}`)
     return await response.json();
   }
 
--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -7,63 +7,56 @@ import { WeatherService } from '../services/weather.service';
   styleUrls: ['./weather-app.component.css']
 })
 export class WeatherAppComponent implements OnInit {
-  @Input() details: any
+  // @Input() details: any
+  date = new Date();
   loading = false;
   city = "";
   region_name = "";
   country_name = "";
-  hour = "";
   weatherNow = {};
-  detailsNextDays = [];
+  feelsLike = "";
+  temp = "";
+  tempMax = "";
+  tempMin = "";
+  windSpd = "";
+  icon = '';
 
   constructor(private weatherService: WeatherService) { }
 
-  startWatch(){
-    const _this = this;
-
-    setInterval(() => {
-      let hour = "";
-      let date = new Date();
-      let hours = date.getHours();
-      let minutes = date.getMinutes();
-      let seconds = date.getSeconds();
-      let hoursFix = hours.toString();
-      if (hours < 10) {
-        hoursFix = "0" + hoursFix;
-      }
-      let minutesFix = minutes.toString();
-      if (minutes < 10) {
-        minutesFix = "0" + minutesFix;
-      }
-      let secondsFix = seconds.toString();
-      if (seconds < 10) {
-        secondsFix = "0" + secondsFix;
-      }
-      _this.hour = `${hoursFix}:${minutesFix}:${secondsFix}`;
-    }, 500);
-  }
-
   async ngOnInit() {
+    // Charging message
     this.loading = true;
+
+    // Get location
     const locationData = await this.weatherService.getLocationData();
     this.city = locationData.city;
     this.region_name = locationData.region;
     this.country_name = locationData.country_name;
     const latitude = locationData.latitude;
     const longitude = locationData.longitude;
-
+    
+    // Get weather by location
     const weatherData = await this.weatherService.getWeatherData(latitude, longitude);
     
-    this.weatherNow = weatherData.data[0].weather.description;
+    // Actual weather data
+    this.weatherNow = weatherData.weather[0].description;
+    this.feelsLike = weatherData.main.feels_like;
+    this.temp = weatherData.main.temp;
+    this.tempMax = weatherData.main.temp_max;
+    this.tempMin = weatherData.main.temp_min;
+    this.windSpd = weatherData.wind.speed;
+    this.icon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    
+    // Icon code test
+    // this.icon = `http://openweathermap.org/img/wn/01n@2x.png`;
+    
     // this.detailsToday = weatherData.dataseries.slice(0, 1)[0];
     // this.detailsNextDays = weatherData.dataseries.slice(1, 5);
 
     this.loading = false;
-    this.startWatch();
     
-    console.log(weatherData);
-    console.log(this.weatherNow);
-    
+    // API CONNECT TEST
+    // console.log(weatherData);
+    // console.log(this.weatherNow);
   }
-
 }
